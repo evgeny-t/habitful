95dbbde83af52d48a8b0e0d815c77ff85b6baf74
etimoshenko <timoshenko.evgeny@gmail.com>
2016-12-14 22:52:22 +0100

    rough draft of drive sync

--- src/js/actions.js
+++ src/js/actions.js
@@ -84,16 +84,62 @@ export function setGoogleAuth2(auth2) {
   };
 }
 
-export const initDriveApiSucceeded = () => {
+export function initDriveApiSucceeded() {
   return {
   };
-};
+}
 
 // https://developers.google.com/drive/v3/web/quickstart/js
 export const initDriveApi = () => {
   return dispatch => {
     gapi.client.load('drive', 'v3', () => {
-      dispatch(exports.initDriveApiSucceeded());
+      dispatch(module.exports.initDriveApiSucceeded());
+
+      dispatch(module.exports.uploadToDrive());
+      setTimeout(() =>
+        dispatch(module.exports.fetchFromDrive())
+        , 1000);
+    });
+  };
+};
+
+export const uploadToDrive = () => {
+  return dispatch => {
+    gapi.client.drive.files.create({
+      resource: {
+        name: 'habitful.json',
+        parents: ['appDataFolder']
+      },
+      media: {
+        mimeType: 'application/json',
+        body: JSON.stringify(module.exports),
+      },
+      fields: 'id',
+    }).then(r => {
+      console.log('r', r);
+    });
+  };
+};
+
+export const fetchFromDrive = () => {
+  return dispatch => {
+    var request = gapi.client.drive.files.list({
+      spaces: ['appDataFolder'],
+      pageSize: 100,
+      fields: 'nextPageToken, files(id, name)',
+    });
+
+    request.execute(function(resp) {
+      console.log('resp:', resp);
+      var files = resp.files;
+      if (files && files.length > 0) {
+        for (var i = 0; i < files.length; i++) {
+          var file = files[i];
+          console.log(`${file.name} ( ${file.id} )`);
+        }
+      } else {
+        console.log('No files found.');
+      }
     });
   };
 };
@@ -105,15 +151,24 @@ export const initDriveApi = () => {
 // avoid calling them `module.exports.func` from the inside of the module.
 
 export function initGoogleAuth() {
+  const scope = [
+    'profile',
+    'email',
+    'https://www.googleapis.com/auth/drive',
+    'https://www.googleapis.com/auth/drive.appfolder',
+  ].join(' ');
+
   return (dispatch) => {
     gapi.load('auth2', () => {
       gapi.auth2.init({
         client_id: '478874198596-bcmpccejmulhe4uf9o04tejtq16pbjmu.apps.googleusercontent.com',
-        scope: 'profile email https://www.googleapis.com/auth/drive',
+        scope,
       }).then(() => {
         const auth2 = gapi.auth2.getAuthInstance();
         if (auth2.isSignedIn.get()) {
           dispatch(module.exports.setUser(auth2.currentUser.get()));
+
+          dispatch(module.exports.initDriveApi());
         }
       });
     });
